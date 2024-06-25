9c3e631c78392f8e8b7bec0cfec37ce59a1f3ac1
Juan Carlos Manzanero Domínguez <juancmandev@protonmail.com>
2024-06-25 15:58:26 -0600

    change build to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,9 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  adapter: vercel(),
   site: "https://www.juancman.dev/",
   integrations: [
     react(),
