befd417f7a19b37769b574f79b4542e6823ec240
Juan Carlos Manzanero Domínguez <juancmandev@protonmail.com>
2024-07-01 22:07:06 -0600

    styles improvements

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,8 +4,8 @@ import formatDate from "@/utils/format-date";
 
 export default function Footer() {
   return (
-    <footer className="border-t border-secondary px-6 py-10 text-center text-sm font-light md:px-16">
-      <section className="space-y-2">
+    <footer className="border-t border-secondary px-6 py-10 text-center text-sm md:px-16 prose prose-invert">
+      <section>
         <p>
           Developed by{" "}
           <strong className="font-bold text-primary">juancmandev</strong>
@@ -23,36 +23,32 @@ export default function Footer() {
             </a>
           </Button>
         </p>
-        <p>Last built {formatDate(new Date())}</p>
+        <p>Last built {formatDate(new Date())}.</p>
       </section>
-      <ul className="mx-auto mt-4 flex max-w-[200px] justify-between">
-        <li>
-          <Button
-            asChild
-            size={null}
-            variant="link"
-            className="flex flex-col justify-center"
-          >
-            <a target="_blank" href="https://github.com/juancmandev/website">
-              <Code className="w-6" />
-              Source Code
-            </a>
-          </Button>
-        </li>
-        <li>
-          <Button
-            asChild
-            size={null}
-            variant="link"
-            className="flex flex-col justify-center"
-          >
-            <a target="_blank" href="https://www.juancman.dev/rss.xml">
-              <RssIcon className="w-6" />
-              RSS feed
-            </a>
-          </Button>
-        </li>
-      </ul>
+      <section className="w-max mx-auto flex items-center gap-12">
+        <Button
+          asChild
+          size={null}
+          variant="link"
+          className="flex flex-col justify-center"
+        >
+          <a target="_blank" href="https://github.com/juancmandev/website">
+            <Code className="w-6" />
+            Source Code
+          </a>
+        </Button>
+        <Button
+          asChild
+          size={null}
+          variant="link"
+          className="flex flex-col justify-center"
+        >
+          <a target="_blank" href="https://www.juancman.dev/rss.xml">
+            <RssIcon className="w-6" />
+            RSS feed
+          </a>
+        </Button>
+      </section>
     </footer>
   );
 }
