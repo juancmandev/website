18c93ff3bb479fad250655307755dbf8940f2a27
juancmandev <juancmandev@protonmail.com>
2025-03-26 11:48:01 -0600

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,26 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import rehypePrettyCode from 'rehype-pretty-code';
 import rehypeSlug from 'rehype-slug';
 import sitemap from '@astrojs/sitemap';
-
 import playformInline from '@playform/inline';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juancman.dev/',
+
   integrations: [
     sitemap(),
     react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
     mdx({
-      syntaxHighlight: false,
-      rehypePlugins: [
-        rehypeSlug,
-        [
-          rehypePrettyCode,
-          {
-            theme: 'catppuccin-mocha',
-          },
-        ],
-      ],
+      syntaxHighlight: 'shiki',
+      shikiConfig: { theme: 'tokyo-night' },
+      rehypePlugins: [rehypeSlug],
     }),
     playformInline(),
   ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
